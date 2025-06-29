2c7788e59e3b19f4ecc247a9225e193017dfce43
Maurosacramento <maurosacramento35@gmail.com>
2025-06-29 16:57:52 +0100

    Finish navBar

---
+++ gerador-certificado/src/app/_components/primary-button/primary-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  templateUrl: './primary-button.component.html',
+  styleUrl: './primary-button.component.css'
+})
+export class PrimaryButtonComponent {
+
+  @Input() label: string = "Primary button"
+}
