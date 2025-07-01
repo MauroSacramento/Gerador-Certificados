cea223fb9cb278d474a929007736e0f2551ce043
Maurosacramento <maurosacramento35@gmail.com>
2025-07-01 13:08:43 +0100

    Navegação

--- gerador-certificado/src/app/pages/certificados/certificados.component.ts
+++ gerador-certificado/src/app/pages/certificados/certificados.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificados',
