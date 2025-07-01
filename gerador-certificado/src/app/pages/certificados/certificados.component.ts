a1ac38abcd59b92f66ba22b8633522710ce78885
Maurosacramento <maurosacramento35@gmail.com>
2025-07-01 22:01:55 +0100

    Validação de formulário

--- gerador-certificado/src/app/pages/certificados/certificados.component.ts
+++ gerador-certificado/src/app/pages/certificados/certificados.component.ts
@@ -3,12 +3,17 @@ import { SecondaryButtonComponent } from "../../_components/secondary-button/sec
 import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
 import { RouterLink } from '@angular/router';
 
+interface CertificateType {
+  name: string,
+  activities: string[]
+}
+
 @Component({
   selector: 'app-certificados',
-  imports: [ItemCertificadoComponent],
+  imports: [ItemCertificadoComponent, SecondaryButtonComponent],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
 export class CertificadosComponent {
-
+  listCertificate: CertificateType[] = []
 }
